cf6db84038744d8035f257c569b8e959112cbfa4
PankajKrana <pankajkumarrana0611@gmail.com>
2024-10-22 14:44:25 +0530

    Animation related fonts
    here it contain the animation regarding the fonts

---
+++ Animate Fonts/Animate Fonts/Animate_FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_FontsApp.swift
+//  Animate Fonts
+//
+//  Created by Pankaj Kumar Rana on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Animate_FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animate Fonts/Animate FontsTests/Animate_FontsTests.swift
@@ -0,0 +1,16 @@
+//
+//  Animate_FontsTests.swift
+//  Animate FontsTests
+//
+//  Created by Pankaj Kumar Rana on 04/10/24.
+//
+
+import Testing
+
+struct Animate_FontsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
