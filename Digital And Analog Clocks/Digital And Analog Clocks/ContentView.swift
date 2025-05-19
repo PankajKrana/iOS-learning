a17b9dc28f8a74f428e0ced8f2c056be5240236c
PankajKrana <pankajkumarrana0611@gmail.com>
2025-05-19 10:20:45 +0530

    initialize the project Digital and Analog Clock

---
+++ Digital And Analog Clocks/Digital And Analog Clocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Digital And Analog Clocks
+//
+//  Created by Pankaj Kumar Rana on 5/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Digital And Analog Clocks/Digital And Analog Clocks/Digital_And_Analog_ClocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_And_Analog_ClocksApp.swift
+//  Digital And Analog Clocks
+//
+//  Created by Pankaj Kumar Rana on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Digital_And_Analog_ClocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
