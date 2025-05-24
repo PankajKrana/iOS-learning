728c335e3629148c6716d858edd9294e38e69610
PankajKrana <pankajkumarrana0611@gmail.com>
2025-05-24 12:17:32 +0530

    added boardview file and updated the code

---
+++ Game Of Life/Game Of Life/View/Board View/CreatureGrid.swift
@@ -0,0 +1,18 @@
+//
+//  CreatureGrid.swift
+//  Game Of Life
+//
+//  Created by Pankaj Kumar Rana on 5/24/25.
+//
+
+import SwiftUI
+
+struct CreatureGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatureGrid()
+}
