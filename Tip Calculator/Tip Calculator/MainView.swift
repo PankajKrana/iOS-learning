3ec28bcc476bd6ef9d763d3f8db0183a48c00da1
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-03 16:04:57 +0530

    initialize the project

---
+++ Tip Calculator/Tip Calculator/MainView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Pankaj Kumar Rana on 6/3/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var isIPad: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+    
+    var isPortraitPhone: Bool {
+        horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    
+    
+    var body: some View {
+        VStack {
+            if isPortraitPhone {
+                Text("Tip Calculator View (portrait)")
+            } else {
+                Text("Tip Calculator View (landscape)")
+
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Tip Calculator/Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Pankaj Kumar Rana on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
