b9a173fbc23264c0bbf75c4b03964b1a25917dbf
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-06 23:30:38 +0530

    added the custom files for the app

---
+++ Tip Calculator/Tip Calculator/Views/Choose Tip View/TipPercentage.swift
@@ -0,0 +1,19 @@
+//
+//  TipPercentage.swift
+//  Tip Calculator
+//
+//  Created by Pankaj Kumar Rana on 6/6/25.
+//
+
+import Foundation
+
+
+enum TipPercentage: Int {
+    case ten = 10
+    case fifteen = 15
+    case twenty = 20
+    case twentyFive = 25
+    case custom = -1
+   
+    
+}
