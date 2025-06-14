f14cf638f5ff331e55b1e1b55dc4dc4ace5b05ca
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-14 23:32:47 +0530

    added to text optionview

--- Flashcard/Flashcard/View/TextAndOptionalImageView.swift
+++ Flashcard/Flashcard/View/TextAndOptionalImageView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct TextAndOptionalImageView: View {
+    let text: String
+    let imageName: ImageResource?
+    let rotate: Bool
+    
+    var titleFont: Font {
+        UIDevice.isIPad ? .largeTitle : .title2
+    }
+    var imageWidthScale: CGFloat {
+        UIDevice.isIPad ? 0.75 : 1.0
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack {
+                Text(.init(text))
+                    .font( titleFont)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                
+                if let imageName {
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width*imageWidthScale)
+                        .frame(maxHeight: geometry.size.height * 0.7)
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+            }
+        }
+        .padding()
+        .rotation3DEffect(
+            rotate ? .degrees(180) : .zero,
+          axis: (x: 0.0, y: 1.0, z: 0.0)
+        )
     }
 }
 
 #Preview {
-    TextAndOptionalImageView()
+    TextAndOptionalImageView(
+        text: "What city is this?",
+        imageName: .bigBen,
+        rotate: false
+    )
 }
