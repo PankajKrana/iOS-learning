7ae975e9bfa76d49ca3b722896e7e9bb1654fd4a
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-14 23:31:49 +0530

    added the textandoptional images view to app

---
+++ Flashcard/Flashcard/View/TextAndOptionalImageView.swift
@@ -0,0 +1,18 @@
+//
+//  TextAndOptionalImageView.swift
+//  Flashcard
+//
+//  Created by Pankaj Kumar Rana on 6/14/25.
+//
+
+import SwiftUI
+
+struct TextAndOptionalImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextAndOptionalImageView()
+}
