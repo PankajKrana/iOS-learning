5e42c2c8d8a10e6271bf829bed045d9b5657038c
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-14 23:40:50 +0530

    added the cardView for the app and iPad support extension

---
+++ Flashcard/Flashcard/Extensions/iPad Support.swift
@@ -0,0 +1,8 @@
+//
+//  iPad Support.swift
+//  Flashcard
+//
+//  Created by Pankaj Kumar Rana on 6/14/25.
+//
+
+import Foundation
---
+++ Flashcard/Flashcard/View/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Flashcard
+//
+//  Created by Pankaj Kumar Rana on 6/14/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
