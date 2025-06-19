01e64569b80ec0ebbfd9876057dc3b98d1c9e11b
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-19 13:48:01 +0530

    added the extension  & fix the main view of the app

--- Mystic Puzzle/Mystic Puzzle/MysticPuzzleView.swift
+++ Mystic Puzzle/Mystic Puzzle/MysticPuzzleView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct MysticPuzzleView: View {
+    @StateObject var mysticPuzzleViewModel: MysticPuzzleViewModel = MysticPuzzleViewModel()
+    let title = "Mystic Puzzle "
+    let titleDimension:CGFloat = 70
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Mystic Puzzle View")
+        ZStack {
+            // background color
+            Color(.blue).opacity(0.3).ignoresSafeArea()
+            
+            VStack {
+                Text( title)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                ZStack {
+                    
+                    let tiles = mysticPuzzleViewModel.mysticPuzzleModel.tiles
+                    
+                    ForEach(tiles) { tile in
+                        TitleView(titleNumber: tile.value, titleDimension: titleDimension, offset: tile.currentPoint)
+                        
+                    }
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     MysticPuzzleView()
+}
+
+struct TitleView: View {
+    let titleNumber: Int
+    let titleDimension: CGFloat
+    let offset: CGPoint
+    let somePadding: CGFloat = 3
+    let digitRasizeFactor: CGFloat = 5/2
+
+    var body: some View {
+        let direction = offset * titleDimension
+        let deltaX = direction.x + titleDimension / 2
+        let deltaY = direction.y - 3 * titleDimension / 2
+        
+        Image("digit\(titleNumber)")
+            .resizable()
+            .renderingMode(.template)
+            .foregroundStyle(.red)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: titleDimension, height: titleDimension)
+            .padding()
+            .background(
+                Color.gray.frame(width: titleDimension - somePadding, height: titleDimension - somePadding)
+                    .cornerRadius(10)
+            )
+            .offset(x: deltaX,
+                    y: deltaY)
+            .shadow(color: .black, radius: 1, x: 1, y: 1)
+
+    }
 }
