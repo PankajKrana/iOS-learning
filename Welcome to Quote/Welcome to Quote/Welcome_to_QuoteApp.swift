6d418ea002bdc38c9161f8d4034ca9c019bf03af
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-22 22:54:33 +0530

    initialize the Project welcome to Quotes

---
+++ Welcome to Quote/Welcome to Quote/Welcome_to_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_to_QuoteApp.swift
+//  Welcome to Quote
+//
+//  Created by Pankaj Kumar Rana on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_to_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
