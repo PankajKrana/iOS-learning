af078271eee90680b0cd12889bae94424cbb85aa
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-24 20:51:43 +0530

    added the welcomeQuotee

---
+++ QuoteADay/QuoteADay/WelcomeQuote.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeQuote.swift
+//  QuoteADay
+//
+//  Created by Pankaj Kumar Rana on 6/24/25.
+//
+
+import SwiftUI
+
+struct WelcomeQuote: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeQuote()
+}
