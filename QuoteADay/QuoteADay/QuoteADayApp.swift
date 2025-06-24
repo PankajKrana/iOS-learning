6a5614e56fd95165ac43c78ff34fd7c164acf5ab
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-24 20:47:11 +0530

    initilize the project of the QuoteAday

---
+++ QuoteADay/QuoteADay/QuoteADayApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteADayApp.swift
+//  QuoteADay
+//
+//  Created by Pankaj Kumar Rana on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct QuoteADayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
