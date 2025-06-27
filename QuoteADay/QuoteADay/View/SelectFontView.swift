46771e8b2584909f1f04a52d5e6f2963fbe36f72
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-27 19:56:23 +0530

    added the custom font and fontview in the app

---
+++ QuoteADay/QuoteADay/View/SelectFontView.swift
@@ -0,0 +1,56 @@
+//
+//  SelectFontView.swift
+//  QuoteADay
+//
+//  Created by Pankaj Kumar Rana on 6/27/25.
+//
+
+import SwiftUI
+
+struct SelectFontView: View {
+    let fontList: [String] = [
+        "Courier New", "Pacifico-Regular", "Molle-Regular", "GreatVibes-Regular",
+        "Sacramento-Regular", "DancingScript-SemiBold", "ButterflyKids-Regular",
+        "GoblinOne", "ChalkboardSE-Bold", "Limelight-Regular", "ShadowsIntoLightTwo-Regular"
+    ]
+    let text: String
+    @Binding var chosenFont: String
+    
+    let textFontSizeConst: CGFloat = 24
+    @State private var color: Color = .blue
+
+    var body: some View {
+        ScrollView(.horizontal,showsIndicators: false) {
+            HStack {
+                ForEach(fontList, id: \.self) { font in
+                    Text(text)
+                        .font(Font.custom(
+                            font,
+                            size: font == chosenFont ? 1.5*textFontSizeConst : textFontSizeConst))
+                        .padding(.horizontal)
+                        .animation(.spring(), value: chosenFont)
+                        .onTapGesture {
+                            chosenFont = font
+                            color = randomColor()
+                        }
+                }
+            }.padding()
+                .background(color.opacity(0.7))
+        }
+    }
+    
+    func randomColor() -> Color {
+        let colorList: [Color] = [
+            .blue,.purple,.pink,.red,.green,.brown
+        ]
+        let index = Int.random(in: 0..<colorList.count)
+        return colorList[index]
+    }
+    
+}
+
+#Preview {
+    SelectFontView(
+        text: "Hello",
+        chosenFont: .constant("Courier New"))
+}
