2547f129b39d6eae628a2ebe670c00828ba09caf
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-28 21:59:04 +0530

    initialize the Meditation app Ui

---
+++ Chill/Chill/ChillApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillApp.swift
+//  Chill
+//
+//  Created by Pankaj Kumar Rana on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ChillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
