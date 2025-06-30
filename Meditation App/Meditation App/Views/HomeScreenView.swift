61605df7d5f7107a0a93437010daec946fbfc789
PankajKrana <pankajkumarrana0611@gmail.com>
2025-06-30 21:40:27 +0530

    added the greetingsView and the chipsView for the app

--- Meditation App/Meditation App/Views/HomeScreenView.swift
+++ Meditation App/Meditation App/Views/HomeScreenView.swift
@@ -22,6 +22,56 @@ struct HomeScreenView: View {
     }
 }
 
+struct ChipsView: View {
+    
+    let chips: [String]
+    @State private var selectedChipIndex = 0
+    
+    var body: some View {
+        HStack {
+            ForEach(0 ..< chips.count) { i in
+                Text(chips[i])
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(
+                        i == selectedChipIndex ? buttonBlue : darkerButtonBlue)
+                    .cornerRadius( 10)
+                    .foregroundColor(textWhite)
+                    .font(.footnote)
+                    .onTapGesture {
+                        selectedChipIndex = i
+                    }
+            }
+            
+            Spacer()
+        }.padding()
+    }
+}
+
+
+struct GreetingsView: View {
+    let name: String
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Good morning, \(name)")
+                    .fontWeight(.semibold)
+                    .foregroundColor(textWhite)
+                    .font(.headline)
+                Text("Have a great day")
+                    .fontWeight(.thin)
+                    .foregroundColor(textWhite)
+                    .font(.subheadline)
+            }
+            Spacer()
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(textWhite)
+                
+        }.padding()
+    }
+}
+
 #Preview {
     HomeScreenView()
 }
