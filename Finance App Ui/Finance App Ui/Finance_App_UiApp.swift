290e8ef1aae4b1bb24e05d13731d4c010d4af843
PankajKrana <pankajkumarrana0611@gmail.com>
2025-07-03 23:07:32 +0530

    initialize the finance app ui

---
+++ Finance App Ui/Finance App Ui/Finance_App_UiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_App_UiApp.swift
+//  Finance App Ui
+//
+//  Created by Pankaj Kumar Rana on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Finance_App_UiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            YourBalanceView()
+        }
+    }
+}
---
+++ Finance App Ui/Finance App Ui/YourBalanceView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance App Ui
+//
+//  Created by Pankaj Kumar Rana on 7/3/25.
+//
+
+import SwiftUI
+
+struct YourBalanceView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    YourBalanceView()
+}
